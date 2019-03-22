e8723fb357adc06582b1ea9e1f0a0dbd49c6bb7b
Kim Jun Woo <junukim.dev@gmail.com>
2019-03-22 16:25:51 +0900

    UPDATE : 간축화

--- natuur/src/container/default/footer/footer.jsx
+++ natuur/src/container/default/footer/footer.jsx
@@ -3,12 +3,10 @@ import React from "react";
 import { FooterContainer } from "../../../styles/Footer";
 import FooterContent from "../../../components/default/Footer/FooterContent";
 
-const Footer = () => {
-  return (
-    <FooterContainer>
-      <FooterContent />
-    </FooterContainer>
-  );
-};
+const Footer = () => (
+  <FooterContainer>
+    <FooterContent />
+  </FooterContainer>
+);
 
 export default Footer;
