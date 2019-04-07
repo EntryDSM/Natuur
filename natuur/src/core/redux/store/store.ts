f32a4bb984bce8b4186ec0140ae716ea67d49840
Kim Jun Woo <junukim.dev@gmail.com>
2019-04-07 23:38:44 +0900

    UPDATE : make redux state type

--- natuur/src/core/redux/store/store.ts
+++ natuur/src/core/redux/store/store.ts
@@ -2,6 +2,9 @@ import { createStore } from "redux";
 
 import reducers from "../reducer";
 
+// redux mapState type
+export type AppState = ReturnType<typeof reducers>;
+
 const devtools =
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
   (window as any).__REDUX_DEVTOOLS_EXTENSION__();
