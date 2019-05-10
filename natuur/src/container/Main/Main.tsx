be3353e1bf676fe2c0d523e65431dd802b58d048
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-10 14:15:00 +0900

    UPDATE: connect updateClassName func

--- natuur/src/container/Main/Main.tsx
+++ natuur/src/container/Main/Main.tsx
@@ -1,34 +1,21 @@
-import React, { FC, useEffect } from "react";
-import { connect } from "react-redux";
-
-import { updateAppContainer } from "../../core/redux/actions/default";
+import React, { FC, useRef, useEffect } from "react";
 
 interface Props {
-  configContainer: typeof updateAppContainer;
+  updateAppClass(text: string): void;
 }
 
-const Main: FC<Props> = ({ configContainer }) => {
-  const didMountRef = React.useRef(false);
+const Main: FC<Props> = ({ updateAppClass }) => {
+  const didMountRef = useRef(false);
 
-  // componentDidmount()
   useEffect(() => {
     if (!didMountRef.current) {
       didMountRef.current = true;
-      // input did mount logic.
-      configContainer({ container: "main-page" });
 
+      updateAppClass("main-page");
       return;
     }
-  });
-
+  }, []);
   return <div>a</div>;
 };
 
-const mapDispatchToProps = dispatch => ({
-  configContainer: payload => dispatch(updateAppContainer(payload))
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Main);
+export default Main;
