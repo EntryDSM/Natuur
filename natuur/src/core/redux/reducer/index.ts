39be27d57a7debef9f672b6481276ae59e7778db
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-14 22:36:13 +0900

    CREATE & UPDATE: add authorizationReducer

--- natuur/src/core/redux/reducer/index.ts
+++ natuur/src/core/redux/reducer/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 import defaultReducer from "./default/defaultReducer";
+import AuthorizationReducer from "./Authorization/AuthorizationReducer";
 
 export default combineReducers({
-  defaultReducer
+  defaultReducer,
+  AuthorizationReducer
   // reducers
 });
