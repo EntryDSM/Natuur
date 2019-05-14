a91f418fad522381ccc0b37991e09ecabebb3247
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-14 22:36:30 +0900

    Update SignUp.tsx

--- natuur/src/container/Authorization/SignUp/SignUp.tsx
+++ natuur/src/container/Authorization/SignUp/SignUp.tsx
@@ -1,17 +1,41 @@
-import React, { FC, useRef, useEffect, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
+import { connect } from "react-redux";
 
+import {
+  AcceptTerms,
+  InformationDisableInputBox,
+  InformationInputBox
+} from "../../../components/Authorization";
+import { HeadLine } from "../../../components/default/Common";
 import {
   Authorization,
   AuthorizationWrapper
 } from "../../../styles/Authorization";
-import { HeadLine } from "../../../components/default/Common";
-import { AcceptTerms, InformationInputBox, InformationDisableInputBox } from "../../../components/Authorization";
+import {
+  updateUserEmail,
+  updateUserPassword,
+  updateUserPasswordCheck
+} from "../../../core/redux/actions/Authorization";
 
 interface Props {
-  updateAppClass(text): void;
+  userEmail: string;
+  userPassword: string;
+  userPasswordCheck: string;
+  updateAppClass(text: string): void;
+  updateUserEmail(targetValue: string): any;
+  updateUserPassword(targetValue: string): void;
+  updateUserPasswordCheck(targetValue: string): void;
 }
 
-const SignUp: FC<Props> = ({ updateAppClass }) => {
+const SignUp: FC<Props> = ({
+  userEmail,
+  userPassword,
+  userPasswordCheck,
+  updateAppClass,
+  updateUserEmail,
+  updateUserPassword,
+  updateUserPasswordCheck
+}) => {
   const didMountRef = useRef(false);
   useEffect(() => {
     if (!didMountRef.current) {
@@ -29,15 +53,65 @@ const SignUp: FC<Props> = ({ updateAppClass }) => {
     setChecked(state);
   };
 
+  // Id, Pw, PwCheck를 store에 dispatch
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "updateUserEmail":
+        updateUserEmail(value);
+        break;
+      case "updateUserPassword":
+        updateUserPassword(value);
+        break;
+      case "updateUserPasswordCheck":
+        updateUserPasswordCheck(value);
+        break;
+      default:
+        return;
+    }
+  };
+
   return (
     <Authorization>
       <AuthorizationWrapper>
         <HeadLine title="2020 지원자 본인인증" />
-        <AcceptTerms isAccept={isAccept} checkedState={checked} />
-        {checked ? <InformationInputBox /> : <InformationDisableInputBox />}
+        <AcceptTerms
+          isAccept={isAccept}
+          checkedState={checked}
+          userEmail={userEmail}
+          userPassword={userPassword}
+          userPasswordCheck={userPasswordCheck}
+        />
+        {checked ? (
+          <InformationInputBox
+            userEmail={userEmail}
+            userPassword={userPassword}
+            userPasswordCheck={userPasswordCheck}
+            handleChange={handleChange}
+          />
+        ) : (
+          <InformationDisableInputBox />
+        )}
       </AuthorizationWrapper>
     </Authorization>
   );
 };
 
-export default SignUp;
+const mapStateToProps = state => ({
+  userEmail: state.AuthorizationReducer.userEmail,
+  userPassword: state.AuthorizationReducer.userPassword,
+  userPasswordCheck: state.AuthorizationReducer.userPasswordCheck
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateUserEmail: targetValue => dispatch(updateUserEmail(targetValue)),
+  updateUserPassword: targetValue => dispatch(updateUserPassword(targetValue)),
+  updateUserPasswordCheck: targetValue =>
+    dispatch(updateUserPasswordCheck(targetValue))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUp);
