e0571f77a8042f44f66126157bb68178fa700eee
jjunu <junukim.dev@gmail.com>
2019-05-17 13:45:49 +0900

    UPDATE: AcceptButton components

--- natuur/src/components/Authorization/AcceptDisableButton.tsx
+++ natuur/src/components/Authorization/AcceptDisableButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 import {
   AcceptButtonComponent,
@@ -7,17 +7,38 @@ import {
   ButtonComponentText
 } from "../../styles/default";
 
-const AcceptDisableButton: FC = () => (
-  <AcceptButtonComponent>
-    <ButtonComponentCover false next as="button">
-      <ButtonComponentArrow false next>
-        〉
-      </ButtonComponentArrow>
-      <ButtonComponentText false next>
-        인증하기
-      </ButtonComponentText>
-    </ButtonComponentCover>
-  </AcceptButtonComponent>
-);
+interface Props {
+  updateToastr(toastrInformation: object): void;
+  removeToastr(): void;
+}
+
+const AcceptDisableButton: FC<Props> = ({ updateToastr, removeToastr }) => {
+  const CreateToastr = () => {
+    removeToastr();
+    updateToastr({
+      timer: 5,
+      toastrMessage: "개인정보를 입력해주세요.",
+      toastrState: "warning"
+    });
+  };
+
+  return (
+    <AcceptButtonComponent>
+      <ButtonComponentCover
+        isDisable={true}
+        next={true}
+        as="button"
+        onClick={() => CreateToastr()}
+      >
+        <ButtonComponentArrow isDisable={true} next={true}>
+          〉
+        </ButtonComponentArrow>
+        <ButtonComponentText isDisable={true} next={true}>
+          인증하기
+        </ButtonComponentText>
+      </ButtonComponentCover>
+    </AcceptButtonComponent>
+  );
+};
 
 export default AcceptDisableButton;
