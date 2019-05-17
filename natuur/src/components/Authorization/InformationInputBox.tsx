5384f748df120792da6727fdd964c28d6f330d20
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-17 13:24:52 +0900

    UPDATE: Transform code into a component.

--- natuur/src/components/Authorization/InformationInputBox.tsx
+++ natuur/src/components/Authorization/InformationInputBox.tsx
@@ -1,95 +1,94 @@
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
 
 import {
   InfomationInputBoxCover,
-  InfomationInputBoxCoverWapper,
-  InfomationInputBoxWapperTitle,
-  InfomationInputBoxWapperInputSpace,
-  InformationInputSpaceArea,
-  InformationInputSpaceCheckMark,
-  InformationInputSpaceWrong,
-  InformationInputSpaceWarning
+  GradationHorizon
 } from "../../styles/Authorization";
 import AcceptButton from "./AcceptButton";
+import InputRow from "../default/Common/InputRow";
 
 interface Props {
   userEmail: string;
   userPassword: string;
   userPasswordCheck: string;
-  handleChange(event: React.ChangeEvent<HTMLInputElement>): void;
+  updateUserEmail(targetValue: string): any;
+  updateUserPassword(targetValue: string): void;
+  updateUserPasswordCheck(targetValue: string): void;
 }
 
 const InformationInputBox: FC<Props> = ({
   userEmail,
   userPassword,
   userPasswordCheck,
-  handleChange
+  updateUserEmail,
+  updateUserPassword,
+  updateUserPasswordCheck
 }) => {
   const emailReg = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   const passwordReg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$/;
+
+  const handleEmail = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+      updateUserEmail(value);
+    },
+    []
+  );
+
+  const handlePassword = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+      updateUserPassword(value);
+    },
+    []
+  );
+
+  const handlePasswordCheck = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+      updateUserPasswordCheck(value);
+    },
+    []
+  );
 
   return (
     <InfomationInputBoxCover>
       <div>
         {/* 이메일 입력란 */}
-        <InfomationInputBoxCoverWapper>
-          <InfomationInputBoxWapperTitle>이메일</InfomationInputBoxWapperTitle>
-          <InfomationInputBoxWapperInputSpace>
-            <InformationInputSpaceArea
-              type="text"
-              placeholder="example@dsmhs.kr"
-              name="updateUserEmail"
-              onChange={event => handleChange(event)}
-            />
-          </InfomationInputBoxWapperInputSpace>
-          {emailReg.test(userEmail) && (
-            <InformationInputSpaceCheckMark>✓</InformationInputSpaceCheckMark>
-          )}
-        </InfomationInputBoxCoverWapper>
+        <InputRow
+          title="이메일"
+          placeHolder="example@dsmhs.kr"
+          name="emailInput"
+          type="text"
+          isCheckMark={emailReg.test(userEmail)}
+          handleChanger={handleEmail}
+        />
+
+        <GradationHorizon />
 
         {/* 비밀번호 입력란 */}
-        <InfomationInputBoxCoverWapper>
-          <InfomationInputBoxWapperTitle>
-            비밀번호
-          </InfomationInputBoxWapperTitle>
-          <InfomationInputBoxWapperInputSpace>
-            <InformationInputSpaceArea
-              type="password"
-              placeholder="••••••••"
-              name="updateUserPassword"
-              onChange={event => handleChange(event)}
-            />
-          </InfomationInputBoxWapperInputSpace>
-          {passwordReg.test(userPassword) && (
-            <InformationInputSpaceCheckMark>✓</InformationInputSpaceCheckMark>
-          )}
-          <InformationInputSpaceWarning>
-            * 영문, 숫자 포함 8자리 이상 16자리 이하
-          </InformationInputSpaceWarning>
-        </InfomationInputBoxCoverWapper>
+        <InputRow
+          title="비밀번호"
+          placeHolder="••••••••"
+          name="passwordInput"
+          type="password"
+          isWarning={true}
+          warningMessage="* 영문, 숫자 포함 8자리 이상 16자리 이하"
+          isCheckMark={passwordReg.test(userPassword)}
+          handleChanger={handlePassword}
+        />
+
+        <GradationHorizon />
 
         {/* 비밀번호 (확인) 입력란 */}
-        <InfomationInputBoxCoverWapper>
-          <InfomationInputBoxWapperTitle>
-            비밀번호 확인
-          </InfomationInputBoxWapperTitle>
-          <InfomationInputBoxWapperInputSpace>
-            <InformationInputSpaceArea
-              type="password"
-              placeholder="••••••••"
-              name="updateUserPasswordCheck"
-              onChange={event => handleChange(event)}
-            />
-            {userPassword === userPasswordCheck && userPasswordCheck && (
-              <InformationInputSpaceCheckMark>✓</InformationInputSpaceCheckMark>
-            )}
-            {userPassword && !userPasswordCheck && (
-              <InformationInputSpaceWrong>
-                비밀번호를 정확히 입력해주세요
-              </InformationInputSpaceWrong>
-            )}
-          </InfomationInputBoxWapperInputSpace>
-        </InfomationInputBoxCoverWapper>
+        <InputRow
+          title="비밀번호 확인"
+          placeHolder="••••••••"
+          name="passwordCheckInput"
+          type="password"
+          isCheckMark={
+            !!(userPassword === userPasswordCheck && userPasswordCheck)
+          }
+          handleChanger={handlePasswordCheck}
+          isWrong={!!(userPassword !== userPasswordCheck && userPassword)}
+        />
       </div>
       <AcceptButton />
     </InfomationInputBoxCover>
