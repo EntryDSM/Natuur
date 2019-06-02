1e97b54fd60bbf935df56b46c06c4ea613dadc23
jjunu <junukim.dev@gmail.com>
2019-06-02 14:50:18 +0900

    FIX: error

--- natuur/src/components/Authorization/PrivacyCheckBox/PrivacyCheckBox.tsx
+++ natuur/src/components/Authorization/PrivacyCheckBox/PrivacyCheckBox.tsx
@@ -37,7 +37,7 @@ const PrivacyCheckBox: FC<Props> = ({
         className={checkedState}
       />
       <PrivacyCheckBoxLabel
-        checkedState={checkedState ? 1 : 0}
+        checkedState={checkedState ? true : false}
         htmlFor="Accept-checkbox"
       >
         ✔
