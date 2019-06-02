da108ca53ab1cdb5be31559fe88a798fc5720f3c
jjunu <junukim.dev@gmail.com>
2019-06-02 13:27:25 +0900

    Update Constance.ts

--- natuur/src/components/default/Constance.ts
+++ natuur/src/components/default/Constance.ts
@@ -7,7 +7,7 @@ export const NOTICE_PAGE_LINK =
   "http://dsmhs.djsch.kr/boardCnts/list.do?boardID=76196&m=0605&s=dsmhs";
 
 // footer
-export const FOOTER_COPYLIGHT = "ⓒ 2018 Entry";
+export const FOOTER_COPYLIGHT = "ⓒ 2019 Entry";
 export const FOOTER_TITLE = "개인정보 처리방침 | 이용약관";
 export const FOOTER_ADDRESS = "(34111)대전광역시 유성구 가정북로 76(장동 23-9)";
 export const FOOTER_OFFICE = "교무실 : 042-866-8822 / Fax : 042-867-9900";
