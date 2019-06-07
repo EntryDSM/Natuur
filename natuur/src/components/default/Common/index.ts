630c1d8141ddaf0435f398727a3a59510817ce42
jjunu <junukim.dev@gmail.com>
2019-06-07 16:15:32 +0900

    CREATE: mainHeadLine component & update index.ts

--- natuur/src/components/default/Common/index.ts
+++ natuur/src/components/default/Common/index.ts
@@ -3,5 +3,13 @@ import ToastrBarCover from "./ToastrBarCover";
 import ToastrBar from "./ToastrBar";
 import InputRow from "./InputRow";
 import TextInput from "./TextInput";
+import MainHeadLine from "./MainHeadLine";
 
-export { HeadLine, ToastrBarCover, ToastrBar, InputRow, TextInput };
+export {
+  HeadLine,
+  ToastrBarCover,
+  ToastrBar,
+  InputRow,
+  TextInput,
+  MainHeadLine
+};
