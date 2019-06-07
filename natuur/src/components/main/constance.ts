2b0b5971c46e80232c24270290f22d8fe8d3b582
jjunu <junukim.dev@gmail.com>
2019-06-07 16:15:36 +0900

    Create constance.ts

---
+++ natuur/src/components/main/constance.ts
@@ -0,0 +1,7 @@
+export const PERIOD_LIST = [
+  { title: "원서작성", date: "0912" },
+  { title: "1차 발표", date: "1030" },
+  { title: "면접", date: "1110" },
+  { title: "2차 발표", date: "1128" },
+  { title: "합격자 등록", date: "1220" }
+];
