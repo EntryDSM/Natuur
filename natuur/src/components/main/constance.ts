d5c25ad96f9a145e46235400e3cc0d27c74386e8
jjunu <junukim.dev@gmail.com>
2019-06-17 22:41:32 +0900

    FIX: merge period list to timestemp list

--- natuur/src/components/main/constance.ts
+++ natuur/src/components/main/constance.ts
@@ -1,9 +1,29 @@
 export const PERIOD_LIST = [
-  { title: "원서작성", date: +new Date("10/24/2019 17:0:0") },
-  { title: "1차 발표", date: +new Date("11/01/2019") },
-  { title: "면접", date: +new Date("11/03/2019") },
-  { title: "2차 발표", date: +new Date("11/07/2019") },
-  { title: "합격자 등록", date: +new Date("11/13/2019 17:0:0") }
+  {
+    endDate: new Date("10/24/2019 17:0:0"),
+    startDate: new Date("10/21/2019 9:0:0"),
+    title: "원서작성"
+  },
+  {
+    endDate: new Date("11/01/2019"),
+    startDate: new Date("10/28/2019"),
+    title: "1차 발표"
+  },
+  {
+    endDate: new Date("11/03/2019"),
+    startDate: new Date("11/01/2019 0:0:1"),
+    title: "면접"
+  },
+  {
+    endDate: new Date("11/07/2019"),
+    startDate: new Date("11/03/2019 0:0:1"),
+    title: "2차 발표"
+  },
+  {
+    endDate: new Date("11/13/2019 17:0:0"),
+    startDate: new Date("11/07/2019"),
+    title: "합격자 등록"
+  }
 ];
 
 export const TIME_STEMP_DATE = [
@@ -12,33 +32,4 @@ export const TIME_STEMP_DATE = [
   "2019.11.01 (금) ~ 2019.11.03 (일)",
   "2019.11.03 (일) ~ 2019.11.07 (목)",
   "2019.11.07 (목) ~ 11.13 (수) 17:00"
-];
-
-export const TIME_STEMP_VIEW_OPEN = [
-  {
-    endDate: new Date("10/24/2019 17:0:0"),
-    startDate: new Date("10/21/2019 9:0:0")
-  },
-  {
-    endDate: new Date("11/01/2019"),
-    startDate: new Date("10/28/2019")
-  },
-  {
-    endDate: new Date("11/03/2019"),
-    startDate: new Date("11/01/2019 0:0:1")
-  },
-  {
-    endDate: new Date("11/07/2019"),
-    startDate: new Date("11/03/2019 0:0:1")
-  },
-  {
-    endDate: new Date("11/13/2019 17:0:0"),
-    startDate: new Date("11/07/2019")
-  }
-];
-
-export const TIME_STEMP_VIEW_CLOSE_FISISH = [
-  new Date("10/28/2019 9:0:0"),
-  new Date("11/03/2019"),
-  new Date("11/07/2019")
 ];
