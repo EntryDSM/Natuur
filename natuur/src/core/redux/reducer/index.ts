f782c58b3354b5374068f9a737a07796cf2aef69
jjunu <junukim.dev@gmail.com>
2019-07-16 22:51:12 +0900

    CREATE & UPDATE: popup connect to redux

--- natuur/src/core/redux/reducer/index.ts
+++ natuur/src/core/redux/reducer/index.ts
@@ -3,10 +3,18 @@ import { combineReducers } from "redux";
 import defaultReducer from "./default/defaultReducer";
 import AuthorizationReducer from "./Authorization/AuthorizationReducer";
 import userReducer from "./User/userReducer";
+import popUpReducer from "./popup/popupReducer";
+import verificationReducer from "./User/verificationReducer";
+import writeNewPasswordReducer from "./User/writeNewPasswordReducer";
+import sendApplicantPasswordReducer from "./User/sendApplicantPasswordReducer";
 
 export default combineReducers({
   AuthorizationReducer,
   defaultReducer,
-  userReducer
+  userReducer,
+  popUpReducer,
+  verificationReducer,
+  writeNewPasswordReducer,
+  sendApplicantPasswordReducer
   // reducers
 });
