60f63306c0e8693dd976ab1fd4fe05a1fa1ef6b5
jjunu <junukim.dev@gmail.com>
2019-07-16 23:18:10 +0900

    UPDATE: connection sagas

--- natuur/src/core/redux/sagas/index.ts
+++ natuur/src/core/redux/sagas/index.ts
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
 
 import user from "./user";
+import verification from "./verification";
 
 export default function* rootSaga() {
-  yield all([call(user)]);
+  yield all([call(user), call(verification)]);
 }
