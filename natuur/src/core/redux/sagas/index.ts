a91b011197b028a8dd138002549365a79f55f300
jjunu <junukim.dev@gmail.com>
2019-07-21 23:47:07 +0900

    UPDATE: add saga to root

--- natuur/src/core/redux/sagas/index.ts
+++ natuur/src/core/redux/sagas/index.ts
@@ -2,7 +2,8 @@ import { all, call } from "redux-saga/effects";
 
 import user from "./user";
 import verification from "./verification";
+import main from "./main";
 
 export default function* rootSaga() {
-  yield all([call(user), call(verification)]);
+  yield all([call(user), call(verification), call(main)]);
 }
