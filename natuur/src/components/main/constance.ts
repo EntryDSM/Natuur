06bffcb449fc67379952bffe77211585b10ee9e6
jjunu <junukim.dev@gmail.com>
2019-07-22 22:45:29 +0900

    UPDATE: fix progress bar

--- natuur/src/components/main/constance.ts
+++ natuur/src/components/main/constance.ts
@@ -6,22 +6,22 @@ export const PERIOD_LIST = [
   },
   {
     endDate: new Date("11/01/2019"),
-    startDate: new Date("10/28/2019"),
+    startDate: new Date("10/28/2019 10:0:0"),
     title: "1차 발표"
   },
   {
-    endDate: new Date("11/03/2019"),
-    startDate: new Date("11/01/2019 0:0:1"),
+    endDate: new Date("11/07/2019"),
+    startDate: new Date("11/01/2019 9:0:0"),
     title: "면접"
   },
   {
-    endDate: new Date("11/07/2019"),
-    startDate: new Date("11/03/2019 0:0:1"),
-    title: "2차 발표"
+    endDate: new Date("11/12/2019 17:0:0"),
+    startDate: new Date("11/07/2019 10:0:0"),
+    title: "발표 및 등록"
   },
   {
-    endDate: new Date("11/13/2019 17:0:0"),
-    startDate: new Date("11/07/2019 0:0:1"),
-    title: "합격자 등록"
+    endDate: new Date("11/23/2019"),
+    startDate: new Date("11/08/2019 0:0:1"),
+    title: "검진결과 제출"
   }
 ];
