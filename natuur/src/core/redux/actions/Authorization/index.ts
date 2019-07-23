ad69d91d196c0354771abf26c5b731be844829fa
jjunu <junukim.dev@gmail.com>
2019-07-23 20:25:32 +0900

    UPDATE: changed from redux to useState.

--- natuur/src/core/redux/actions/Authorization/index.ts
+++ natuur/src/core/redux/actions/Authorization/index.ts
@@ -1,48 +1,5 @@
-// reducerState
-export interface RootState {
-  userEmail: string;
-  userPassword: string;
-  userPasswordCheck: string;
-}
-
 // Types
-export const UPDATE_USER_EMAIL = "UPDATE_USER_EMAIL";
-export const UPDATE_USER_PASSWORD = "UPDATE_USER_PASSWORD";
-export const UPDATE_USER_PASSWORD_CHECK = "UPDATE_USER_PASSWORD_CHECK";
-
-interface UpdateUserEmail {
-  type: string;
-  payload: string;
-}
-
-interface UpdateUserPassword {
-  type: string;
-  payload: string;
-}
-
-interface UpdateUserPasswordCheck {
-  type: string;
-  payload: string;
-}
-
-export type AuthorizationTypes =
-  | UpdateUserEmail
-  | UpdateUserPassword
-  | UpdateUserPasswordCheck
-  | null;
 
 // Actions
-export const updateUserEmail = (payload: string) => ({
-  type: UPDATE_USER_EMAIL,
-  payload
-});
 
-export const updateUserPassword = (payload: string) => ({
-  type: UPDATE_USER_PASSWORD,
-  payload
-});
-
-export const updateUserPasswordCheck = (payload: string) => ({
-  type: UPDATE_USER_PASSWORD_CHECK,
-  payload
-});
+export type AuthorizationTypes = null;
