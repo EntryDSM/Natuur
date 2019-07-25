6a1bffbed808a1ff95eee0a894eaa7d140811ee3
jjunu <junukim.dev@gmail.com>
2019-07-25 12:18:18 +0900

    UPDATE & CREATE: connect to server with saga

--- natuur/src/core/redux/reducer/index.ts
+++ natuur/src/core/redux/reducer/index.ts
@@ -7,7 +7,7 @@ import popUpReducer from "./popup/popupReducer";
 import verificationReducer from "./User/verificationReducer";
 import writeNewPasswordReducer from "./User/writeNewPasswordReducer";
 import sendApplicantPasswordReducer from "./User/sendApplicantPasswordReducer";
-import mainReducer from 'main/mainReducer';
+// import mainReducer from 'main/mainReducer';
 
 export default combineReducers({
   AuthorizationReducer,
@@ -16,7 +16,7 @@ export default combineReducers({
   popUpReducer,
   verificationReducer,
   writeNewPasswordReducer,
-  sendApplicantPasswordReducer,
-  mainReducer
+  sendApplicantPasswordReducer
+  // mainReducer
   // reducers
 });
