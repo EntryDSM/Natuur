fe5d45dd4e1d384afd6055ae62959b27bab2cc2d
jjunu <junukim.dev@gmail.com>
2019-07-26 23:17:53 +0900

    UPDATE: connect to redux with sagas

--- natuur/src/core/redux/actions/main/index.ts
+++ natuur/src/core/redux/actions/main/index.ts
@@ -4,9 +4,13 @@ export const GET_USER_APPLICANT_STATUS_SUCCESS =
   "GET_USER_APPLICANT_STATUS_SUCCESS";
 export const GET_USER_APPLICANT_STATUS_FAILURE =
   "GET_USER_APPLICANT_STATUS_FAILURE";
+export const GET_USER_APPLICANT_INFOMATION = "GET_USER_APPLICANT_INFOMATION";
+export const GET_USER_APPLICANT_INFOMATION_SUCCESS =
+  "GET_USER_APPLICANT_INFOMATION_SUCCESS";
+export const GET_USER_APPLICANT_INFOMATION_FAILURE =
+  "GET_USER_APPLICANT_INFOMATION_FAILURE";
 
-export interface UserApplicantStatusType {
-  accessToken: string;
+export interface UserApplicantStatusApiType {
   isPaid?: boolean;
   isPassedFirstApply?: boolean;
   isPassedFinalApply?: boolean;
@@ -14,6 +18,9 @@ export interface UserApplicantStatusType {
   isFinalSubmit?: boolean;
   receiptCode?: number;
   examCode?: number;
+}
+interface UserApplicantStatusType extends UserApplicantStatusApiType {
+  accessToken: string;
 }
 export interface UserApplicantStatus {
   type:
@@ -23,7 +30,31 @@ export interface UserApplicantStatus {
   payload: UserApplicantStatusType;
 }
 
-export type MainActionTypes = UserApplicantStatus | null;
+export interface UserApplicantInfoApiType {
+  email?: string;
+  applicantName?: string;
+  sex?: string;
+  birthDate?: string;
+  parentName?: string;
+  parentTel?: string;
+  applicantTel?: string;
+  address?: string;
+  postCode?: number;
+  imagePath?: string;
+}
+interface UserApplicantInfoType extends UserApplicantInfoApiType {
+  email: string;
+  accessToken: string;
+}
+export interface UserApplicantInfo {
+  type:
+    | typeof GET_USER_APPLICANT_INFOMATION
+    | typeof GET_USER_APPLICANT_INFOMATION_SUCCESS
+    | typeof GET_USER_APPLICANT_INFOMATION_FAILURE;
+  payload: UserApplicantInfoType;
+}
+
+export type MainActionTypes = UserApplicantStatus | UserApplicantInfo | null;
 
 // actions
 export const getUserApplicantStatus = (
@@ -31,4 +62,11 @@ export const getUserApplicantStatus = (
 ): MainActionTypes => ({
   payload,
   type: GET_USER_APPLICANT_STATUS
+});
+
+export const getUserApplicantInfo = (
+  payload: UserApplicantInfoType
+): MainActionTypes => ({
+  payload,
+  type: GET_USER_APPLICANT_INFOMATION
 });
