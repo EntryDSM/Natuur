5424ab2aeef71d7cfe519a84677107648fea714a
jjunu <junukim.dev@gmail.com>
2019-07-31 22:35:58 +0900

    UPDATE: fix typo

--- natuur/src/container/default/header/Header.tsx
+++ natuur/src/container/default/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 import Navigation from "../../../components/default/Header/Navigation";
-import { HeaderNav } from "../../../styles/Header";
+import * as S from "../../../styles/Header";
 
 interface OwnProps {
   accessToken: string;
@@ -10,12 +10,12 @@ interface OwnProps {
 
 const Header: FC<OwnProps> = ({ accessToken, userName }) => {
   return (
-    <HeaderNav>
+    <S.HeaderNav>
       <Navigation
         isActive={accessToken !== "" ? true : false}
         userName={userName}
       />
-    </HeaderNav>
+    </S.HeaderNav>
   );
 };
 
