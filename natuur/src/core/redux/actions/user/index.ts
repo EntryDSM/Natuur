0c5e20eb5d9969171f9fbc73f1e3274be16b59cd
jjunu <junukim.dev@gmail.com>
2019-08-26 22:22:47 +0900

    UPDATE: token refresh redux logic

--- natuur/src/core/redux/actions/user/index.ts
+++ natuur/src/core/redux/actions/user/index.ts
@@ -2,8 +2,15 @@
 export const LOG_IN = "LOG_IN";
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
+export const LOG_OUT = "LOG_OUT";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURS = "LOG_OUT_FAILURS";
+export const REFRESH_JWT = "REFRESH_JWT";
+export const REFRESH_JWT_SUCCESS = "REFRESH_JWT_SUCCESS";
+export const REFRESH_JWT_FAILURS = "REFRESH_JWT_FAILURS";
 export const RESET_STATE = "RESET_STATE";
 export const GET_USER_EMAIL = "GET_USER_EMAIL";
+export const CHECK_ACCESS_TOKEN = "CHECK_ACCESS_TOKEN";
 
 interface LogInPayload {
   email: string;
@@ -11,10 +18,7 @@ interface LogInPayload {
   access?: string;
   refresh?: string;
 }
-export interface LogIn {
-  type: typeof LOG_IN | typeof LOG_IN_SUCCESS | typeof LOG_IN_FAILURE;
-  payload: LogInPayload;
-}
+
 interface ResetState {
   type: typeof RESET_STATE;
 }
@@ -22,9 +26,35 @@ interface GetUserEmail {
   type: typeof GET_USER_EMAIL;
   payload: { userEmail: string };
 }
+export interface LogIn {
+  type: typeof LOG_IN | typeof LOG_IN_SUCCESS | typeof LOG_IN_FAILURE;
+  payload: LogInPayload;
+}
+export interface LogOut {
+  type: typeof LOG_OUT | typeof LOG_OUT_SUCCESS | typeof LOG_OUT_FAILURS;
+  payload: { refreshToken: string };
+}
+export interface RefreshJWT {
+  type:
+    | typeof REFRESH_JWT
+    | typeof REFRESH_JWT_SUCCESS
+    | typeof REFRESH_JWT_FAILURS;
+  payload: { refreshToken: string; accessToken?: string };
+}
+export interface CheckAccessToken {
+  type: typeof CHECK_ACCESS_TOKEN;
+  payload: { isAccess: boolean };
+}
 
 // Actions
-export type UserActionTypes = LogIn | ResetState | GetUserEmail | null;
+export type UserActionTypes =
+  | LogIn
+  | ResetState
+  | GetUserEmail
+  | LogOut
+  | RefreshJWT
+  | CheckAccessToken
+  | null;
 
 export const logIn = (payload: LogInPayload): UserActionTypes => ({
   payload,
@@ -40,4 +70,23 @@ export const getUserEmail = (payload: {
 }): UserActionTypes => ({
   payload,
   type: GET_USER_EMAIL
+});
+
+export const logOut = (payload: { refreshToken: string }): UserActionTypes => ({
+  payload,
+  type: LOG_OUT
+});
+
+export const refreshJWT = (payload: {
+  refreshToken: string;
+}): UserActionTypes => ({
+  payload,
+  type: REFRESH_JWT
+});
+
+export const checkAccessToken = (payload: {
+  isAccess: boolean;
+}): UserActionTypes => ({
+  payload,
+  type: CHECK_ACCESS_TOKEN
 });
