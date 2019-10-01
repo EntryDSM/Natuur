9480a4e1ffa6385993cd59a6e8f254deed1033f9
jjunu <junukim.dev@gmail.com>
2019-10-01 12:19:16 +0900

    UPDATE: change intro api with redux pattern

--- natuur/src/container/Introduce/Introduce.tsx
+++ natuur/src/container/Introduce/Introduce.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC } from "react";
 import { connect } from "react-redux";
 
 import * as S from "../../styles/intro";
@@ -10,7 +10,6 @@ import {
 } from "../../components/intro/constance";
 import Pagination from "../../components/default/pagination/Pagination";
 import {
-  getDocument,
   patchDocument,
   setSelfIntroduction,
   setStudyPlan,
@@ -20,15 +19,11 @@ import { AppState } from "../../core/redux/store/store";
 
 const mapStateToProps = (state: AppState) => ({
   accessToken: state.userReducer.accessToken,
-  isSuccess: state.introReducer.isSuccess,
-  isError: state.introReducer.isError,
   selfIntroduction: state.introReducer.selfIntroduction,
   studyPlan: state.introReducer.studyPlan
 });
 
 const mapDispatchToProps = dispatch => ({
-  getDocument: (payload: { accessToken: string }) =>
-    dispatch(getDocument(payload)),
   patchDocument: (payload: PatchDocumentType) =>
     dispatch(patchDocument(payload)),
   setSelfIntroduction: (payload: { text: string }) =>
@@ -41,63 +36,42 @@ type Props = ReturnType<typeof mapStateToProps> &
 
 const Introduce: FC<Props> = ({
   accessToken,
-  isSuccess,
-  isError,
   selfIntroduction,
   studyPlan,
-  getDocument,
   patchDocument,
   setSelfIntroduction,
   setStudyPlan
-}) => {
-  const didMountRef = useRef(false);
-  useEffect(() => {
-    if (!didMountRef.current) {
-      didMountRef.current = true;
-
-      getDocument({ accessToken });
-    }
-  },        []);
-
-  return (
-    <div>
-      <S.IntroWrapper>
-        <HeadLine
-          subText="2020 입학원서 작성"
-          title={`자기소개서 & 학업계획서 입력`}
-        />
-        <PlanTemplate
-          title="자기소개서"
-          description={SELF_INTRODUCTION_DESCRIPTION}
-          value={selfIntroduction}
-          setValue={setSelfIntroduction}
-          patchDocument={patchDocument}
-          accessToken={accessToken}
-        />
-        <PlanTemplate
-          title="학업계획서"
-          description={STUDY_PLAN_DESCRIPTION}
-          value={studyPlan}
-          setValue={setStudyPlan}
-          patchDocument={patchDocument}
-          accessToken={accessToken}
-        />
-        <Pagination
-          connectServer={() =>
-            patchDocument({
-              accessToken,
-              self_introduction_text: selfIntroduction,
-              study_plan_text: studyPlan
-            })
-          }
-          prevRouterPath="/grade"
-          nextRouterPath="/preview"
-          AcceptPagination="intro"
-        />
-      </S.IntroWrapper>
-    </div>
-  );
-};
+}) => (
+  <div>
+    <S.IntroWrapper>
+      <HeadLine
+        subText="2020 입학원서 작성"
+        title={`자기소개서 & 학업계획서 입력`}
+      />
+      <PlanTemplate
+        title="자기소개서"
+        description={SELF_INTRODUCTION_DESCRIPTION}
+        value={selfIntroduction}
+        setValue={setSelfIntroduction}
+        patchDocument={patchDocument}
+        accessToken={accessToken}
+      />
+      <PlanTemplate
+        title="학업계획서"
+        description={STUDY_PLAN_DESCRIPTION}
+        value={studyPlan}
+        setValue={setStudyPlan}
+        patchDocument={patchDocument}
+        accessToken={accessToken}
+      />
+      <Pagination
+        prevRouterPath="/grade"
+        nextRouterPath="/preview"
+        AcceptPagination="intro"
+      />
+    </S.IntroWrapper>
+  </div>
+);
 
 export default connect(
   mapStateToProps,
--- natuur/src/core/redux/sagas/intro/index.ts
+++ natuur/src/core/redux/sagas/intro/index.ts
@@ -1,64 +1,64 @@
-import { all, fork, takeLatest, put, call } from "redux-saga/effects";
+// import { all, fork, takeLatest, put, call } from "redux-saga/effects";
 
-import {
-  GET_DOCUMENT,
-  GET_DOCUMENT_FAILURE,
-  GET_DOCUMENT_SUCCESS,
-  PATCH_DOCUMENT,
-  PATCH_DOCUMENT_FAILURE,
-  PATCH_DOCUMENT_SUCCESS,
-  GetDocument,
-  PatchDocument,
-  DocumentApiType
-} from "../../actions/intro";
-import { getUserDocumentApi, patchUserDocumentApi } from "../../../../lib/api";
-import { tokenRefresh } from "../token";
+// import {
+//   GET_DOCUMENT,
+//   GET_DOCUMENT_FAILURE,
+//   GET_DOCUMENT_SUCCESS,
+//   PATCH_DOCUMENT,
+//   PATCH_DOCUMENT_FAILURE,
+//   PATCH_DOCUMENT_SUCCESS,
+//   GetDocument,
+//   PatchDocument,
+//   DocumentApiType
+// } from "../../actions/intro";
+// import { getUserDocumentApi, patchUserDocumentApi } from "../../../../lib/api";
+// import { tokenRefresh } from "../token";
 
-function* getUserDocument(action: GetDocument) {
-  try {
-    const response: DocumentApiType = yield call(
-      getUserDocumentApi,
-      action.payload
-    );
-    yield put({ type: GET_DOCUMENT_SUCCESS, payload: response });
-  } catch (e) {
-    if (e.response.status === 401) {
-      yield tokenRefresh(
-        getUserDocumentApi,
-        action.payload,
-        GET_DOCUMENT_SUCCESS,
-        GET_DOCUMENT_FAILURE
-      );
-    }
-    yield put({ type: GET_DOCUMENT_FAILURE });
-  }
-}
+// function* getUserDocument(action: GetDocument) {
+//   try {
+//     const response: DocumentApiType = yield call(
+//       getUserDocumentApi,
+//       action.payload
+//     );
+//     yield put({ type: GET_DOCUMENT_SUCCESS, payload: response });
+//   } catch (e) {
+//     if (e.response.status === 401) {
+//       yield tokenRefresh(
+//         getUserDocumentApi,
+//         action.payload,
+//         GET_DOCUMENT_SUCCESS,
+//         GET_DOCUMENT_FAILURE
+//       );
+//     }
+//     yield put({ type: GET_DOCUMENT_FAILURE });
+//   }
+// }
 
-function* watchGetUserDocument() {
-  yield takeLatest(GET_DOCUMENT, getUserDocument);
-}
+// function* watchGetUserDocument() {
+//   yield takeLatest(GET_DOCUMENT, getUserDocument);
+// }
 
-function* patchUserDocument(action: PatchDocument) {
-  try {
-    yield call(patchUserDocumentApi, action.payload);
-    yield put({ type: PATCH_DOCUMENT_SUCCESS });
-  } catch (e) {
-    if (e.response.status === 401) {
-      yield tokenRefresh(
-        patchUserDocumentApi,
-        action.payload,
-        PATCH_DOCUMENT_SUCCESS,
-        PATCH_DOCUMENT_FAILURE
-      );
-    }
-    yield put({ type: PATCH_DOCUMENT_FAILURE });
-  }
-}
+// function* patchUserDocument(action: PatchDocument) {
+//   try {
+//     yield call(patchUserDocumentApi, action.payload);
+//     yield put({ type: PATCH_DOCUMENT_SUCCESS });
+//   } catch (e) {
+//     if (e.response.status === 401) {
+//       yield tokenRefresh(
+//         patchUserDocumentApi,
+//         action.payload,
+//         PATCH_DOCUMENT_SUCCESS,
+//         PATCH_DOCUMENT_FAILURE
+//       );
+//     }
+//     yield put({ type: PATCH_DOCUMENT_FAILURE });
+//   }
+// }
 
-function* watchPatchUserDocument() {
-  yield takeLatest(PATCH_DOCUMENT, patchUserDocument);
-}
+// function* watchPatchUserDocument() {
+//   yield takeLatest(PATCH_DOCUMENT, patchUserDocument);
+// }
 
-export default function* introSaga() {
-  yield all([fork(watchGetUserDocument), fork(watchPatchUserDocument)]);
-}
+// export default function* introSaga() {
+//   yield all([fork(watchGetUserDocument), fork(watchPatchUserDocument)]);
+// }
