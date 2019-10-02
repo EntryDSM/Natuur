591133cc134516318d6d79fe506c68710b55d4a9
jjunu <junukim.dev@gmail.com>
2019-10-02 15:21:27 +0900

    UPDATE: Process data into intro page.

--- natuur/src/container/Introduce/Introduce.tsx
+++ natuur/src/container/Introduce/Introduce.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 
 import * as S from "../../styles/intro";
@@ -15,12 +15,16 @@ import {
   setStudyPlan,
   PatchDocumentType
 } from "../../core/redux/actions/intro";
+import { setIsOpen } from "../../core/redux/actions/default";
 import { AppState } from "../../core/redux/store/store";
 
 const mapStateToProps = (state: AppState) => ({
   accessToken: state.userReducer.accessToken,
   selfIntroduction: state.introReducer.selfIntroduction,
-  studyPlan: state.introReducer.studyPlan
+  studyPlan: state.introReducer.studyPlan,
+  isOpen: state.defaultReducer.isOpen,
+  selfIntroductionAndStudyPlan:
+    state.applicantDocument.self_introduction_and_study_plan
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -28,7 +32,11 @@ const mapDispatchToProps = dispatch => ({
     dispatch(patchDocument(payload)),
   setSelfIntroduction: (payload: { text: string }) =>
     dispatch(setSelfIntroduction(payload)),
-  setStudyPlan: (payload: { text: string }) => dispatch(setStudyPlan(payload))
+  setStudyPlan: (payload: { text: string }) => dispatch(setStudyPlan(payload)),
+  setIsOpen: (payload: {
+    pageName: "info" | "personal" | "grade" | "intro";
+    isOpen: boolean;
+  }) => dispatch(setIsOpen(payload))
 });
 
 type Props = ReturnType<typeof mapStateToProps> &
@@ -40,38 +48,58 @@ const Introduce: FC<Props> = ({
   studyPlan,
   patchDocument,
   setSelfIntroduction,
-  setStudyPlan
-}) => (
-  <div>
-    <S.IntroWrapper>
-      <HeadLine
-        subText="2020 입학원서 작성"
-        title={`자기소개서 & 학업계획서 입력`}
-      />
-      <PlanTemplate
-        title="자기소개서"
-        description={SELF_INTRODUCTION_DESCRIPTION}
-        value={selfIntroduction}
-        setValue={setSelfIntroduction}
-        patchDocument={patchDocument}
-        accessToken={accessToken}
-      />
-      <PlanTemplate
-        title="학업계획서"
-        description={STUDY_PLAN_DESCRIPTION}
-        value={studyPlan}
-        setValue={setStudyPlan}
-        patchDocument={patchDocument}
-        accessToken={accessToken}
-      />
-      <Pagination
-        prevRouterPath="/grade"
-        nextRouterPath="/preview"
-        AcceptPagination="intro"
-      />
-    </S.IntroWrapper>
-  </div>
-);
+  setStudyPlan,
+  selfIntroductionAndStudyPlan,
+  isOpen,
+  setIsOpen
+}) => {
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      const { self_introduction, study_plan } = selfIntroductionAndStudyPlan;
+
+      if (!isOpen.intro) {
+        setSelfIntroduction({ text: self_introduction });
+        setStudyPlan({ text: study_plan });
+      }
+    }
+    return () => setIsOpen({ pageName: "intro", isOpen: true });
+  },        []);
+
+  return (
+    <div>
+      <S.IntroWrapper>
+        <HeadLine
+          subText="2020 입학원서 작성"
+          title={`자기소개서 & 학업계획서 입력`}
+        />
+        <PlanTemplate
+          title="자기소개서"
+          description={SELF_INTRODUCTION_DESCRIPTION}
+          value={selfIntroduction}
+          setValue={setSelfIntroduction}
+          patchDocument={patchDocument}
+          accessToken={accessToken}
+        />
+        <PlanTemplate
+          title="학업계획서"
+          description={STUDY_PLAN_DESCRIPTION}
+          value={studyPlan}
+          setValue={setStudyPlan}
+          patchDocument={patchDocument}
+          accessToken={accessToken}
+        />
+        <Pagination
+          prevRouterPath="/grade"
+          nextRouterPath="/preview"
+          AcceptPagination="intro"
+        />
+      </S.IntroWrapper>
+    </div>
+  );
+};
 
 export default connect(
   mapStateToProps,
