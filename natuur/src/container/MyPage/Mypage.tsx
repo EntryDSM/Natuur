cb6d028e4dc52964148cb7c87f0e59dd92c4dced
jjunu <junukim.dev@gmail.com>
2019-10-07 14:46:23 +0900

    UPDATE: dispatch get applicant

--- natuur/src/container/MyPage/Mypage.tsx
+++ natuur/src/container/MyPage/Mypage.tsx
@@ -1,21 +1,29 @@
 import React, { FC, useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import HeadLine from "../../components/default/Common/HeadLine";
 import * as S from "../../styles/mypage";
 import RowList from "../../components/mypage/RowList";
 import PageList from "../../components/mypage/PageList";
+import { getUserApplicantStatus } from "../../core/redux/actions/main/index";
+import { AppState } from "../../core/redux/store/store";
 
 interface OwnProps {
   updateAppClass: (className: string) => void;
 }
 
 const MyPage: FC<OwnProps> = ({ updateAppClass }) => {
-  const didMountRef = useRef(false);
+  const dispatch = useDispatch();
+  const accessToken = useSelector<AppState, string>(
+    state => state.userReducer.accessToken
+  );
 
+  const didMountRef = useRef(false);
   useEffect(() => {
     if (!didMountRef.current) {
       didMountRef.current = true;
 
+      dispatch(getUserApplicantStatus({ accessToken }));
       updateAppClass("my-page");
     }
   },        []);
