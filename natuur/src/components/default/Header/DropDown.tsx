71db21ebb7e07279cd4c0d80113053f4dab8b090
jjunu <junukim.dev@gmail.com>
2019-10-08 23:09:10 +0900

    UDPATE: add login push action

--- natuur/src/components/default/Header/DropDown.tsx
+++ natuur/src/components/default/Header/DropDown.tsx
@@ -1,22 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
+import { useDispatch } from "react-redux";
 
 import * as S from "../../../styles/Header";
+import { updateToastr } from "../../../core/redux/actions/default";
 
 interface OwnProps {
   refreshToken: string;
   logOut: (payload: { refreshToken: string }) => void;
 }
 
-const DropDown: FC<OwnProps> = ({ refreshToken, logOut }) => (
-  <S.HeaerDropDownBox>
-    <div>
-      <S.Button to="/mypage">마이페이지</S.Button>
-      <S.Horizon />
-      <S.Button onClick={() => logOut({ refreshToken })} to="/" as="button">
-        로그아웃
-      </S.Button>
-    </div>
-  </S.HeaerDropDownBox>
-);
+const DropDown: FC<OwnProps> = ({ refreshToken, logOut }) => {
+  const dispatch = useDispatch();
+
+  const createToastr = useCallback(() => {
+    dispatch(
+      updateToastr({
+        timer: 5,
+        toastrMessage: "로그아웃 되었습니다.",
+        toastrState: "success"
+      })
+    );
+  },                               []);
+
+  const logOutHandler = useCallback(() => {
+    logOut({ refreshToken });
+    createToastr();
+  },                                [refreshToken]);
+
+  return (
+    <S.HeaerDropDownBox>
+      <div>
+        <S.Button to="/mypage">마이페이지</S.Button>
+        <S.Horizon />
+        <S.Button onClick={logOutHandler} to="/">
+          로그아웃
+        </S.Button>
+      </div>
+    </S.HeaerDropDownBox>
+  );
+};
 
 export default DropDown;
